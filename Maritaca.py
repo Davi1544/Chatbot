989fe6800ecb48ea97b208adf9581c72753ea618
nattan ferreira <125780771+nattan447@users.noreply.github.com>
2024-10-03 20:46:30 -0300

    Maritaca devolvendo palavras com base no feedback

--- Maritaca.py
+++ Maritaca.py
@@ -1,53 +1,54 @@
 import maritalk
 
+
 class Maritaca:
- 
-    
     model = 0
-    
-    def __init__(self, key,prompt ):
-          '''
+
+    def __init__(self, key, prompt):
+        '''
             key : Chave da api do bot;
             prompt: prompt few-shot maritaca;
             '''
 
-          self.__key = key
-          self.prompt  = prompt
-    
+        self.__key = key
+        self.prompt = prompt
+
     def start_maritaca(self):
         '''
-        essa função inicializa o modelo da maritaca;
-        '''
-        global model    
-        model = maritalk.MariTalk(
-            key=self.__key,
-            model="sabia-3" 
-        )
-    
-
-    def accumulatePrompt(self,tip):
-        '''
-        essa função faz o bot chutar uma palavra com base na dica passda , e retorna essa palavra;
-        tip : (String) dica passada para o bot advinhar a palavra;
-        return : (String) retorna a palavra que o bot chutou;
-
+            essa função inicializa o modelo da maritaca;
         '''
         global model
-        
-        answer = model.generate(self.prompt, max_tokens=200)["answer"]
-        
-        self.prompt+= tip + "\n Resposta:" 
+        model = maritalk.MariTalk(
+            key=self.__key,
+            model="sabia-3"
+        )
 
-        answer = model.generate(self.prompt, max_tokens=200)["answer"]
+    def getAnswer(self, feedback, previous_answer):
+        ''' essa funcao recebe um feedback e com base no feedback   faz  e
+        o bot chutar uma palavra, e retorna essa palavra.
+        previous_answer : (String) palavra anterior chutada pelo bot.
+        feedback : (String) feedback para ser transformado em dicas
+        return : (String) retorna a palavra que o bot chutou; '''
+        tip = ""  # dica que vai ser passado para o bot
 
-        self.prompt+=  answer  + "\n Dica:"
-                
+        # se não tiver feedback , não possuo dicas
+        if len(feedback) > 0:
+            for i in range(len(feedback)):
+                if feedback[i] == "Y":
+                    tip += f"A palavra possui a letra  '{previous_answer[i]}' na posição {i + 1} da palavra."
+                elif feedback[i] == "N":
+                    tip += f"A palavra não possui a letra  '{previous_answer[i]}'."
+                elif feedback[i] == "M":
+                    tip += f"A palavra  possui a letra '{previous_answer[i]}',porém está na posição errada."
+        else:
+            tip = "Não há dicas disponíveis ainda."
+
+        if tip == "Não há dicas disponíveis ainda.":
+            self.prompt += tip + "\n Resposta:"
+            answer = model.generate(self.prompt, max_tokens=200)["answer"]
+            self.prompt += answer + "\n Dica:"
+        else:
+            self.prompt += tip + "\n Resposta:"
+            answer = model.generate(self.prompt, max_tokens=200)["answer"]
+            self.prompt += answer + "\n Dica:"
         return answer
-
-
-
-
-
-
-
-
